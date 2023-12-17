11317209e738ba2992695472ba344c3eb04b14fd
Manaday Mavani <manadaym@users.noreply.github.com>
2023-12-18 00:57:04 +0530

    chore(Card): adds props and mock data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,32 @@
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-
-function Card() {
+function Card({ data }) {
   return (
-    <div className="relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden">
+    <div className="relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden">
       {/* icon */}
       <FaRegFileAlt />
       {/* text */}
-      <p className="text-sm leading-tight font-semibold mt-5">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <p className="text-sm leading-tight font-semibold mt-5">{data.desc}</p>
       {/* footer */}
       <div className="footer absolute bottom-0 w-full left-0">
         {/* footer/data */}
         <div className="flex items-center justify-between px-8 py-3 mb-3">
-          <h5>.4mb</h5>
+          <h5>{data.filesize}</h5>
           <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
-            <LuDownload size=".7em" color="#fff" />
+            {data.close ? <IoClose size=".7em" color="#fff" /> : <LuDownload size=".7em" color="#fff" />}
           </span>
         </div>
         {/* footer/tag */}
-        <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-          <h3 className="text-sm font-semibold">Download Now</h3>
-        </div>
+        {
+          data.tag.isOpen && (
+            <div className={`tag w-full py-4 ${data.tag.tagColor} flex items-center justify-center`}>
+              <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+            </div>
+          )
+        }
+
       </div>
     </div>
   )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,9 +1,47 @@
 import Card from './Card'
 
 function Foreground() {
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".8mb",
+      close: false,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "bg-green-600"
+      }
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".8mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "bg-blue-600"
+      }
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".8mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "bg-green-600"
+      }
+    },
+  ];
+
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-      <Card></Card>
+    <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+      {data.map((item, index) => {
+        return (
+          <Card data={item} />
+        )
+      })}
     </div>
   )
 }
