9745e027702e4ead2ea0e50464d864d872b2d374
Manaday Mavani <manadaym@users.noreply.github.com>
2023-12-17 23:53:43 +0530

    chore(Card): add file icon at top

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
+import { FaRegFileAlt } from "react-icons/fa";
+
 function Card() {
   return (
-    <div className="w-60 h-72 rounded-[20px] bg-zinc-900/90">
+    <div className="w-60 h-72 rounded-[20px] bg-zinc-900/90 text-white p-5">
       {/* icon */}
+      <FaRegFileAlt />
       {/* text */}
       {/* footer */}
       {/* footer/data */}
