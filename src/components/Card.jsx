8322547cf36ccd26260a6c798e3621e49ee6a6bf
Manaday Mavani <manadaym@users.noreply.github.com>
2023-12-17 23:51:30 +0530

    chore(Card): adds a list of required child components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,12 @@
 function Card() {
   return (
-    <div className="w-60 h-72 rounded-[20px] bg-zinc-900/90"></div>
+    <div className="w-60 h-72 rounded-[20px] bg-zinc-900/90">
+      {/* icon */}
+      {/* text */}
+      {/* footer */}
+      {/* footer/data */}
+      {/* footer/tag */}
+    </div>
   )
 }
 
