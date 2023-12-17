249985fda1e9a4329a62876f1be19e704266f52d
Manaday Mavani <manadaym@users.noreply.github.com>
2023-12-17 23:47:08 +0530

    chore(Card): adds dumb Card component

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,6 +1,10 @@
+import Card from './Card'
+
 function Foreground() {
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'></div>
+    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+      <Card></Card>
+    </div>
   )
 }
 
